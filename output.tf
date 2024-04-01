b4814a9b47515973ea7add4c165d664404eb2a56
sn0rlax <79283899+sn0rlaxlife@users.noreply.github.com>
2024-04-01 14:45:20 -0700

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "object" {
+  value = azurerm_bastion_host.bastion
+}
+
+output "name" {
+  value = azurerm_bastion_host.bastion.name
+}
+
+output "id" {
+  value = azurerm_bastion_host.bastion.id
+}
