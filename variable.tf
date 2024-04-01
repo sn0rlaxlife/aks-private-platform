1e32d17e064780cd1b1bf2adf32365e78cf4bd53
sn0rlax <79283899+sn0rlaxlife@users.noreply.github.com>
2024-04-01 14:45:49 -0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,11 +3,19 @@ variable "vnet" {
   type = object({
     cidr_block     = string
     subnet_cluster = string
+    subnet_api_server = string
     location       = string
   })
   default = {
     cidr_block     = "10.240.0.0/16"
     subnet_cluster = "10.240.0.0/22"
+    subnet_api_server = "10.240.4.0/22"
     location       = "eastus"
   }
+}
+
+variable "resource_group" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "aks-platform-private-rg"
 }
