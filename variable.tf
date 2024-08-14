320f7a83a86142b54630c0a216cf8cf98aea5d22
sn0rlax <79283899+sn0rlaxlife@users.noreply.github.com>
2024-08-14 14:35:09 -0700

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -14,8 +14,35 @@ variable "vnet" {
   }
 }
 
-variable "resource_group" {
+variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
   default     = "aks-platform-private-rg"
+}
+
+# The name of the subnet for accessing API Server this will be peered with our Virtual Network for segmented access
+variable "subnet_name" {
+  description = "The name of the subnet for accessing API Server"
+  type        = string
+  default     = "aks-subnet-network"
+}
+
+# Subnet that houses the NAT Gateway for outbound access
+variable "subnet_name_aks" {
+  description = "The name of the subnet for accessing AKS"
+  type        = string
+  default     = "subnet-aks-private"
+}
+
+# Virtual Network Name of the AKS Cluster that has to be assigned for Network Contributor Role
+variable "vnet_name" {
+  description = "The Virtaul Network Name of the AKS Cluster that has to be assigned for Network Contributor Role"
+  type        = string
+  default     = "aks-private-vnet"
+}
+
+# Due to scoping of the assignment this will need to be required in a .tfvars (workaround for now - have this as .gitignore don't commit)
+variable "subscription_id" {
+  description = "The Azure Subscription ID"
+  type        = string
 }
